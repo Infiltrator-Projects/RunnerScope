d8f0309f224f16dfb9234bb29f42183cab578e79
Shannon Smith <infiltratr@yandex.com>
2026-09-04 08:48:52 +1000

    Add Linux launcher

---
+++ runnerscope_linux.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Shannon Smith
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""Linux launcher for RunnerScope."""
+from runnerscope import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
