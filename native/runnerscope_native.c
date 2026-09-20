5ce450c2faf61259cd8bff6dacd6b4d9509f509e
Shannon Smith <infiltratr@yandex.com>
2026-09-20 18:54:38 +1000

    Bump Runner Monitor native bridge to 1.1.10

--- native/runnerscope_native.c
+++ native/runnerscope_native.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define RUNNERSCOPE_VERSION "1.1.9"
+#define RUNNERSCOPE_VERSION "1.1.10"
 #define RUNNERSCOPE_COMMON_VERSION "1.19.10"
 
 static InfiltratrProjectInfo project_info(void)
