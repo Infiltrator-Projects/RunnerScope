52410d398ee8b9b3bf2040ab51b3c04f7fb3e33a
Shannon Smith <infiltratr@yandex.com>
2026-09-21 15:40:45 +1000

    Remove obsolete runnerscope_linux.py compatibility product

--- runnerscope_linux.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (C) 1993-2026 Shannon Smith
-# SPDX-License-Identifier: GPL-3.0-or-later
-"""Linux launcher for Runner Monitor."""
-from runnerscope import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
